208a771956a5e294618ad83c6cb4870ba1d07d7f
unknown <advisorsbiztech@gmail.com>
2025-05-08 00:30:07 +0530

    first commit1

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,20 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Real State location",
-  description: "Real state project",
-};
 
 export default function RootLayout({ children }) {
   return (
